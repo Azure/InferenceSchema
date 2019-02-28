3930f3a6fac8c01cbb9f724490db5f820d55ad98
trangevi <trangevi@microsoft.com>
2019-02-28 15:44:32 -0800

    Removing imports from init file

--- inference_schema/parameter_types/__init__.py
+++ inference_schema/parameter_types/__init__.py
@@ -1,8 +1,3 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
-
-from .numpy_parameter_type import NumpyParameterType
-from .pandas_parameter_type import PandasParameterType
-from .spark_parameter_type import SparkParameterType
-from .standard_py_parameter_type import StandardPythonParameterType
