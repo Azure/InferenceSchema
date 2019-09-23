7752e80793e8dadc74a46be6402af6a962ee5ec7
trangevi <trangevi@microsoft.com>
2019-09-23 10:56:48 -0700

    Adding Some tests. Adding classes to init.

--- inference_schema/parameter_types/__init__.py
+++ inference_schema/parameter_types/__init__.py
@@ -1,3 +1,15 @@
 # ---------------------------------------------------------
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # ---------------------------------------------------------
+
+from .numpy_parameter_type import NumpyParameterType
+from .pandas_parameter_type import PandasParameterType
+from .spark_parameter_type import SparkParameterType
+from .standard_py_parameter_type import StandardPythonParameterType
+
+__all__ = [
+    'NumpyParameterType',
+    'PandasParameterType',
+    'SparkParameterType',
+    'StandardPythonParameterType'
+]
