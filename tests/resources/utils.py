f410cbc463b20ebd571c08d216307ef5568cc3b0
t-tiye <t-tiye@microsoft>
2020-08-10 15:55:47 +0800

    update docstring

--- tests/resources/utils.py
+++ tests/resources/utils.py
@@ -1,4 +1,16 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
 def ordered(obj):
+    """
+    sort all items iteratively if nested
+
+    :param obj:
+    :type func: nested item | dict or list
+    :return:
+    :rtype: obj
+    """
     if isinstance(obj, dict):
         return sorted((k, ordered(v)) for k, v in obj.items())
     if isinstance(obj, list):
