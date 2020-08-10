08d4532fbb5d40ef972953f27b0aaa09ce2e6baf
t-tiye <t-tiye@microsoft>
2020-08-10 13:54:53 +0800

    test utils

---
+++ tests/resources/utils.py
@@ -0,0 +1,7 @@
+def ordered(obj):
+    if isinstance(obj, dict):
+        return sorted((k, ordered(v)) for k, v in obj.items())
+    if isinstance(obj, list):
+        return sorted(ordered(x) for x in obj)
+    else:
+        return obj
