ba461749009de47e22259d72f13072feb0724555
Neehar Duvvuri <neduvvur@microsoft.com>
2021-12-01 12:17:12 -0500

    Add format to numpy and pandas parameter types

--- inference_schema/parameter_types/_constants.py
+++ inference_schema/parameter_types/_constants.py
@@ -7,3 +7,6 @@ DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S.%f %z"
 TIME_FORMAT = "%H:%M:%S.%f %z"
 ERR_PYTHON_DATA_NOT_JSON_SERIALIZABLE = "Invalid python data sample provided: ensure that the data is fully JSON " \
                                         "serializable to be able to generate swagger schema from it. Actual error: {}"
+class SWAGGER_FORMAT_CONSTANTS:
+    NUMPY_FORMAT = "numpy.ndarray"
+    PANDAS_FORMAT = "pandas.DataFrame"
