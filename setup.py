27f83a3176093a5a7125ad00e9581caa3d83edac
Travis Angevine <trangevi@microsoft.com>
2023-07-21 14:40:53 -0700

    Drop python 3.7 support

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ CLASSIFIERS = [
     'Intended Audience :: Developers',
     'Intended Audience :: System Administrators',
     'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
     'Programming Language :: Python :: 3.9',
     'Programming Language :: Python :: 3.10',
